63a5457e17f64dd00c12faab982af7f3803e5cd7
Ghizmilth <hidanerferrer@gmail.com>
2020-07-21 18:59:12 -0700

    Added some style to the app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import "./App.css";
 import { Searchbar, Cards } from './components';
 import { fetchWeather } from './api';
-
+import {Container, Typography} from "@material-ui/core";
 
 function App() {
 
-    // This is used to get info from Searchbar and pass it to Cards component
     const [ selectedCity, setSelectedCity ] = useState([]);
 
 
@@ -14,13 +13,17 @@ function App() {
         setSelectedCity(await fetchWeather(newCity));
     };
 
-    console.log(selectedCity);
-
       return (
-        <div className="App">
-          <Searchbar updateSelectedCity={updateSelectedCity} />
-          <Cards selectedCity={selectedCity}/>
-        </div>
+          <div className="App">
+          <Container maxWidth="md">
+                  <Typography variant="h3"  gutterBottom>
+                      Check the weather in any city in the world!
+                  </Typography>
+                  <Searchbar updateSelectedCity={updateSelectedCity} />
+              <Cards selectedCity={selectedCity}/>
+
+        </Container>
+          </div>
     )
 }
 
